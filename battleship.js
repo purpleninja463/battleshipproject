91dbb38108079cebd66c5b788b0227fa47638b9e
Lucas S <sushiguy132@gmail.com>
2016-09-07 09:16:42 -0700

    Game can now fire and hit target

--- battleship.js
+++ battleship.js
@@ -30,15 +30,18 @@ for (i = 0; i < cols; i++) {
 		var square = document.createElement("div");
 		gameBoardContainer.appendChild(square);
 
+		square.textContent = letterArray[j] + (i + 1);
+
     // give each div element a unique id based on its row and column, like "s00"
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
 		// set each grid square's coordinates: multiples of the current row or column number
-		
-			square.textContent = i+1+
-		letterArray[j]
+
+
+
+
 
 		var topPosition = j * squareSize;
 		var leftPosition = i * squareSize;
@@ -65,7 +68,22 @@ var gameBoard = [
 
 
 function fireTorpedo() {
-
 	// Your game logic will go here!
+	torpedo = document.getElementById("fireInput").value;
+	x = torpedo.substring(0, 1);
+	row = letterConversion[x];
+	column = torpedo.substring(1, 3);
+
+	if (gameBoard[row][column - 1] == 1){
+
+	   document.getElementById("s" + row + (column - 1)).style.background =  "red";
+
+	   }
+	   else {
+
+	      document.getElementById("s" + row + (column - 1)).style.background = "grey";
+
+	       }
+	        var userInput = $("fireInput").val();
 
 }
